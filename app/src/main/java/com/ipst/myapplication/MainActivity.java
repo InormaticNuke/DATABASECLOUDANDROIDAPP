e426c133f6a4410113e7928f1b9d75900b662757
Nuke <InformaticNuke@gmail.com>
2023-11-21 21:49:11 -0300

    0.005.1

--- app/src/main/java/com/ipst/myapplication/MainActivity.java
+++ app/src/main/java/com/ipst/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.ipst.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -32,6 +33,20 @@ public class MainActivity extends AppCompatActivity {
         txtID = findViewById(R.id.mascotID);
         txtName = findViewById(R.id.mascotName);
         txtOwner =findViewById(R.id.mascotOwner);
-        txtAdress = findViewById(R.id.mas)
+        txtAdress = findViewById(R.id.ownerAdress);
+
+        spMascot = findViewById(spMascot);
+        list = findViewById(R.id.listShow);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                MascotType);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spMascot.setAdapter(adapter);
+
+    }
+
+    public void LoadListFirestore(){
+
     }
 }
